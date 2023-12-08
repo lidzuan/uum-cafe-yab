34ca6d373c481b910a2945b78a7a2e36ab5e2929
Lee Juan (280027) <144795292+lidzuan@users.noreply.github.com>
2023-12-08 20:15:26 +0800

    Add files via upload

---
+++ CrysReport/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ds As New UUMCafeDatabase5DataSet
+        Dim da As New UUMCafeDatabase5DataSetTableAdapters.PaymentTableAdapter
+        da.Fill(ds.Payment)
+        Dim rpt As New CrystalReport1
+        rpt.SetDataSource(ds)
+        CrystalReportViewer1.ReportSource = rpt
+
+    End Sub
+End Class
